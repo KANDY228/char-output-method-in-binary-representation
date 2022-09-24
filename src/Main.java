7f26c8adfd357c96aad1e89a2dfd99697cad9bae
KANDY228 <71720867+KANDY228@users.noreply.github.com>
2022-09-24 19:37:32 +0300

    char output method in binary representation
    char output method in binary representation

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		String a = Integer.toBinaryString('a');
+		System.out.println(a);
+		
+		String b = Integer.toBinaryString('b');
+		System.out.println(b);
+		
+		String c = Integer.toBinaryString('c');
+		System.out.println(c);
+	}
+
+}
